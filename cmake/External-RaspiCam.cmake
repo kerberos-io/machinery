270526450e357c4385107ccf4f2cd9508d9152fd
Cédric Verstraeten <hello@cedric.ws>
2016-01-29 20:35:26 +0100

    fix static compilation of raspicam

--- cmake/External-RaspiCam.cmake
+++ cmake/External-RaspiCam.cmake
@@ -24,12 +24,13 @@ ExternalProject_Add(raspicamera
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/thirdparty
     -DBUILD_SHARED_LIBS=OFF
+    -DBUILD_UTILS=OFF
 )
 
 set(RASPBERRYPI_INCLUDE_DIR ${CMAKE_BINARY_DIR}/thirdparty/include/)
 set(RASPBERRYPI_LIBRARY_DIR ${CMAKE_BINARY_DIR}/thirdparty/lib/)
 
+set(RASPBERRYPI_LIBRARIES raspicam.a raspicam_cv.a mmal mmal_core mmal_util)
+
 include_directories(${RASPBERRYPI_INCLUDE_DIR})
 link_directories(${RASPBERRYPI_LIBRARY_DIR})
-
-set(RASPBERRYPI_LIBRARIES raspicam raspicam_cv mmal mmal_core mmal_util)

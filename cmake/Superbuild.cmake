2ca96addbda5be891c62e940ac2fadfc63956938
Cédric Verstraeten <hello@cedric.ws>
2016-03-30 21:56:32 +0200

    included easylogging directly in project

--- cmake/Superbuild.cmake
+++ cmake/Superbuild.cmake
@@ -24,14 +24,6 @@ include( ExternalProject )
         set(KERBEROS_TEST_DEPENDENCIES ${KERBEROS_TEST_DEPENDENCIES} googletest)
     endif()
 
-
-
-# --------------------------------------------------------------------------
-# Logging with easylogging
-
-    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/External-Easylogging.cmake)
-    set(KERBEROS_DEPENDENCIES ${KERBEROS_TEST_DEPENDENCIES} easylogging)
-
 # --------------------------------------------------------------------------
 # OpenCV
 

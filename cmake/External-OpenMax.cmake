896ea4ed0da6d809e19af4479fa6af894b2ddab7
Cédric Verstraeten <cedric.verstraeten@me.com>
2017-06-10 22:28:32 +0200

    Create External-OpenMax.cmake

--- cmake/External-OpenMax.cmake
+++ cmake/External-OpenMax.cmake
@@ -1,7 +1,7 @@
 # -----------------------------------
 # Userland, ARM used by raspicam
 
-message("Bind project: Userland")
+message("Bind project: OpenMAX")
 
 set(USERLAND_INCLUDE_DIR /opt/vc/include/)
 set(USERLAND_LIBRARY_DIR /opt/vc/lib/)
@@ -24,7 +24,8 @@ ExternalProject_Add(openmax
   INSTALL_COMMAND ""
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/thirdparty
-    -DBUILD_SHARED_LIBS=OFF
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/../../host/usr/share/buildroot/toolchainfile.cmake
+    -DBUILD_SHARED_LIBS=OFF   
 )
 
 set(OPENMAX_INCLUDE_DIR ${CMAKE_BINARY_DIR}/openmax/include/)

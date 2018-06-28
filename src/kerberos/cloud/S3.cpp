46c5e16387ac70c14a1df9d8c9edacc9c824059e
Cédric Verstraeten <hello@cedric.ws>
2018-06-28 14:18:40 +0200

    add kerberos proxy, to verify request before it's send to S3

--- src/kerberos/cloud/S3.cpp
+++ src/kerberos/cloud/S3.cpp
@@ -83,7 +83,7 @@ namespace kerberos
 
         // -----------------------------
         // Set storage type
-        
+
         canonicalizedAmzHeaders.push_back("x-amz-storage-class:ONEZONE_IA");
 
         // ------------------------------------------------
@@ -235,10 +235,12 @@ namespace kerberos
             curl_easy_setopt(curlHandle, CURLOPT_HEADER, true);//Set header true
             curl_easy_setopt(curlHandle, CURLOPT_HTTPHEADER, httpHeaders);//Set headers
             curl_easy_setopt(curlHandle, CURLOPT_URL, url.c_str());//Set URL
+            curl_easy_setopt(curlHandle, CURLOPT_PROXY, "http://proxy.kerberos.io:80");
             curl_easy_setopt(curlHandle, CURLOPT_TRANSFER_ENCODING, 1L);
             curl_easy_setopt(curlHandle, CURLOPT_CUSTOMREQUEST, "PUT");
             curl_easy_setopt(curlHandle, CURLOPT_UPLOAD, 1L);
             curl_easy_setopt(curlHandle, CURLOPT_READDATA, fd);
+            curl_easy_setopt(curlHandle, CURLOPT_SSL_VERIFYPEER, 0L);
             curl_easy_setopt(curlHandle, CURLOPT_NOSIGNAL, 1L);
             curl_easy_setopt(curlHandle, CURLOPT_READFUNCTION, reader);
             curl_easy_setopt(curlHandle, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_info.st_size);
